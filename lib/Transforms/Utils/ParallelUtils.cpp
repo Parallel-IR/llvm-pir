6db727ed75ef3c91c821c20393542dd61dc82af2
Bran Hagger <bran.hagger@outlook.com>
2016-08-19 09:30:27 -0700

    Add handling of fork interior to the sequentialize parallel regions pass.

--- lib/Transforms/Utils/ParallelUtils.cpp
+++ lib/Transforms/Utils/ParallelUtils.cpp
@@ -11,11 +11,13 @@
 //
 //===----------------------------------------------------------------------===//
 
-#include "llvm/Analysis/ParallelRegionInfo.h"
 #include "llvm/Transforms/Utils/ParallelUtils.h"
 
 #include "llvm/IR/Instructions.h"
+#include "llvm/IR/Constants.h"
 #include "llvm/Transforms/Utils/BasicBlockUtils.h"
+#include "llvm/Transforms/Utils/Cloning.h"
+#include "llvm/Transforms/Utils/ValueMapper.h"
 #include "llvm/ADT/DenseMap.h"
 #include "llvm/ADT/DenseSet.h"
 
@@ -32,162 +34,324 @@ SequentializeParallelRegions::SequentializeParallelRegions() :
 
 SequentializeParallelRegions::~SequentializeParallelRegions() {}
 
-bool SequentializeParallelRegions::runOnFunction(Function &F) {
-  ParallelRegionInfo *PRI =
-      &(getAnalysis<ParallelRegionInfoPass>().getParallelRegionInfo());
-  if(PRI->TopLevelRegions.empty()) {
-    return false;
-  }
-
-  auto DT = &getAnalysis<DominatorTreeWrapperPass>().getDomTree();
-
-  std::deque<BasicBlock *> OpenBlocks;
-  std::deque<ParallelRegion *> OpenRegions;
-  DenseMap<BasicBlock *, unsigned> Exit2Entry;
-  DenseMap<ParallelRegion *, unsigned> Region2EntryJoin;
-  DenseSet<BasicBlock *> SeenBlocks;
-
-  BasicBlock* BB;
-  ParallelRegion* PR;
-
-  // Figure out which exit block ends which entry block.
-  for (ParallelRegion *SubRegion : PRI->TopLevelRegions) {
-    OpenRegions.push_back(SubRegion);
-  }
-
-  // TODO: Handle fork interior
-  while(!OpenRegions.empty()) {
-    PR = OpenRegions.back();
-    OpenRegions.pop_back();
-
-    for(ParallelRegion *SubRegion : PR->getSubRegions()) {
-      OpenRegions.push_back(SubRegion);
+static inline unsigned minDepth(BasicBlock *BB,
+                                DenseMap<BasicBlock*, unsigned> &Block2Depth) {
+  unsigned minDepth = UINT_MAX;
+  for (pred_iterator PI = pred_begin(BB), E = pred_end(BB);
+       PI != E; ++PI) {
+    if (Block2Depth.count(*PI) && Block2Depth[*PI] < minDepth) {
+      minDepth = Block2Depth[*PI];
     }
+  }
 
-    SmallVectorImpl<BasicBlock *> &Entrys = PR->getEntryBlocks();
-    for (unsigned i = 0; i < Entrys.size(); ++i) {
-      if (isa<JoinInst>(Entrys[i]->begin())) {
-        assert(!Region2EntryJoin.count(PR) && "Do not handle duplicate fork"
-                                              "destinations yet");
-        // TODO: Remove additional branches that go directly to join.
-        // Easier if removeTask interface for ForkInst is modified.
-        Region2EntryJoin[PR] = i;
+  if (minDepth == UINT_MAX) {
+    return 0;
+  }
+
+  return minDepth + 1;
+}
+
+static inline void remapInstruction(Instruction *I,
+                                    DenseSet<BasicBlock *> &PrevBlocks,
+                                    DenseSet<BasicBlock*> &NewBlocks,
+                                    ValueToValueMapTy &VMap,
+                                    DenseMap<BasicBlock*, unsigned> &Blck2D){
+  PHINode *PN = dyn_cast<PHINode>(I);
+  if (PN) {
+    for (unsigned idx = 0; idx < PN->getNumIncomingValues(); ++idx) {
+      BasicBlock *Incoming = PN->getIncomingBlock(idx);
+      ValueToValueMapTy::iterator InIt = VMap.find(Incoming);
+      if (InIt != VMap.end()) {
+        Incoming = cast<BasicBlock>(InIt->second);
+        if (PN->getBasicBlockIndex(Incoming) >= 0) {
+          PN->removeIncomingValue(idx);
+          --idx;
+          continue;
+        }
+        PN->setIncomingBlock(idx, Incoming);
+      } else if (!PrevBlocks.count(Incoming) && !NewBlocks.count(Incoming)) {
+        PN->removeIncomingValue(idx);
+        --idx;
+        continue;
       }
-    }
-
-    if (!Region2EntryJoin.count(PR)) {
-      Region2EntryJoin[PR] = 0;
-    }
-  
-    for (BasicBlock *Exit : PR->getExitBlocks()) {
-      for (unsigned i = 0; i < Entrys.size(); ++i) {
-        if (DT->dominates(Entrys[i], Exit)) {
-          Exit2Entry[Exit] = i;
+      
+      ValueToValueMapTy::iterator ValIt = VMap.find(PN->getIncomingValue(idx));
+      if (ValIt != VMap.end()) {
+        Instruction *Val = dyn_cast<Instruction>(ValIt->second);
+        if (!Val ||
+            (!Blck2D.count(Val->getParent()) || (
+             Blck2D[Incoming] >= Blck2D[Val->getParent()]))) {
+          PN->setIncomingValue(idx, ValIt->second);
         }
       }
-      assert(Exit2Entry.count(Exit) || isa<ForkInst>(Exit->getTerminator()));
-
-      if (isa<HaltInst>(Exit->getTerminator()) && 
-          Region2EntryJoin[PR] == Exit2Entry[Exit]) {
-        Region2EntryJoin[PR] = (Region2EntryJoin[PR] + 1) % Entrys.size();
+    }
+  } else {
+    for (unsigned op = 0, E = I->getNumOperands(); op != E; ++op) {
+      Value *Op = I->getOperand(op);
+      ValueToValueMapTy::iterator It = VMap.find(Op);
+      if (It != VMap.end()) {
+        I->setOperand(op, It->second);
       }
     }
   }
+}
+
+// All basic blocks must be copied!
+// Returns the Basic Block executed after the parallel region.
+BasicBlock *SequentializeRegion(BasicBlock *regionStart) {
+  SmallVector<BasicBlock *, 4> EntryBlocks;
+  SmallVector<BasicBlock *, 4> OrigEntrys;
+
+  // Keep track of clone values for remapping.
+  // Each entry point has its own value map.
+  SmallVector<ValueToValueMapTy *, 4> ValueMaps;
+
+  // Blocks remaining to be processed.
+  std::deque<BasicBlock *> OpenBlocks;
+
+  // Prevent infinite loops by keeping track of where we've been.
+  DenseSet<BasicBlock *> VisitedBlocks;
+
+  // Keep track of clone blocks whose instructions need to be remapped.
+  DenseSet<BasicBlock *> NewBlocks;
+
+  // These are the only basic blocks that know about copies from a different
+  // entry point.
+  DenseSet<BasicBlock *> Prev;
+  Prev.insert(regionStart);
+
+  // Keep track of what depth in the tree copies are.
+  // TODO: This is a hack to prevent overwriting a PHI node to use something
+  // that does not dominate its use. Either prove this is a valid way of
+  // precisely avoiding such situations or, better, update dominator information
+  // and use that instead.
+  DenseMap<BasicBlock *, unsigned> Block2Depth;
+
+  // Used as exit if every path ends in a halt.
+  BasicBlock *exit = BasicBlock::Create(regionStart->getContext(), "AllHalt",
+                                        regionStart->getParent());
+  new UnreachableInst(regionStart->getContext(), exit);
+
+  ForkInst *regTerm = dyn_cast<ForkInst>(regionStart->getTerminator());
+  assert(regTerm && "Should only be called on a block that ends with a fork.");
+
+  // First, copy all of the entry blocks.
+  for (unsigned i = 0; i < regTerm->getNumSuccessors(); ++i) {
+    BasicBlock *successor = regTerm->getSuccessor(i);
+    if (successor == regionStart ||
+        (isa<JoinInst>(successor->getTerminator()) &&
+         isa<UnreachableInst>(exit->getTerminator()))) {
+      exit = successor;
+    } else if (!isa<JoinInst>(successor->getTerminator())) {
+      if (isa<ForkInst>(successor->getTerminator())) {
+        SequentializeRegion(successor);
+      }
+      ValueToValueMapTy *vmap = new ValueToValueMapTy();
+      BasicBlock *newEntry = CloneBasicBlock(successor, *vmap,
+                                            "forkChild");
+      regionStart->getParent()->getBasicBlockList().push_back(newEntry);
+      EntryBlocks.push_back(newEntry);
+      OrigEntrys.push_back(successor);
+      ValueMaps.push_back(vmap);
+    }
+  }
+
+  for (BasicBlock *OrigEntry : OrigEntrys) {
+    OrigEntry->removePredecessor(regionStart, true);
+  }
+ 
+  // Then, handle all of the children.
+  for (unsigned i = 0; i < EntryBlocks.size(); ++i) {
+    BasicBlock *BB = EntryBlocks[i];
+    ValueToValueMapTy *vmap = ValueMaps[i];
+    BasicBlock *end = exit;
+    if (i < EntryBlocks.size() - 1) {
+      end = EntryBlocks[i + 1];
+    }
+
+    OpenBlocks.clear();
+    NewBlocks.clear();
+    Block2Depth.clear();
+    VisitedBlocks.clear();
+
+    VisitedBlocks.insert(regionStart);
+    VisitedBlocks.insert(OrigEntrys[i]);
+    VisitedBlocks.insert(BB);
+    VisitedBlocks.insert(end);
+
+    (*vmap)[OrigEntrys[i]] = BB;
+    Block2Depth[BB] = 1;
+    Block2Depth[OrigEntrys[i]] = 1;
+    Block2Depth[regionStart] = 0;
+
+    NewBlocks.insert(BB);
+    OpenBlocks.push_back(BB);
+    while (!OpenBlocks.empty()) {
+      BB = OpenBlocks.front();
+      OpenBlocks.pop_front();
+
+      if (isa<HaltInst>(BB->getTerminator())) {
+        if (!isa<UnreachableInst>(end->getTerminator()) ||
+            !regTerm->isInterior()) {
+          ReplaceInstWithInst(BB->getTerminator(), BranchInst::Create(end));
+          if (i < EntryBlocks.size()) {
+            Prev.insert(BB);
+            PHINode *PN;
+            for (BasicBlock::iterator I = end->begin(); 
+                 (PN = dyn_cast<PHINode>(I)); ++I) {
+              PN->addIncoming(PN->getIncomingValueForBlock(regionStart), BB);
+            }
+          }
+        }
+        continue;
+      }
+
+      TerminatorInst *Term = BB->getTerminator();
+      for (unsigned idx = 0; idx < Term->getNumSuccessors(); ++idx) {
+        BasicBlock *successor = Term->getSuccessor(idx);
+        TerminatorInst *succTerm = successor->getTerminator();
+
+        if (successor == regionStart || (isa<JoinInst>(succTerm) && 
+             isa<UnreachableInst>(exit->getTerminator()))) {
+          // For fork interior, delay the join so the next call up on the
+          // stack can deal with it correctly.
+          if (successor != regionStart && regTerm->isInterior()) {
+            exit = BasicBlock::Create(successor->getContext(), "delayJoin",
+                                      successor->getParent(), successor);
+            BranchInst::Create(successor, exit);
+            Term->setSuccessor(idx, exit);
+          } else {
+            exit = successor;
+          }
+        } else if (!isa<JoinInst>(succTerm) &&
+                   VisitedBlocks.insert(successor).second) {
+          ForkInst *SubFork = dyn_cast<ForkInst>(successor->getTerminator());
+          if (SubFork) {
+            // Any already made copies of successors children will not know
+            // the block has rewired their uses.
+            for (BasicBlock *SubSucc : successors(successor)) {
+              if (vmap->count(SubSucc)) {
+                BasicBlock *copy = dyn_cast<BasicBlock>((*vmap)[SubSucc]);
+                if (copy) {
+                  copy->removePredecessor(successor, true);
+                }
+              }
+            }
+            SequentializeRegion(successor);
+          }
+
+          BasicBlock *newSuccessor = CloneBasicBlock(successor, *vmap);
+          regionStart->getParent()->getBasicBlockList().push_back(newSuccessor);
+          (*vmap)[successor] = newSuccessor;
+          NewBlocks.insert(newSuccessor);
+          OpenBlocks.push_back(newSuccessor);
+
+          unsigned depth = minDepth(successor, Block2Depth);
+          Block2Depth[successor] = depth;
+          Block2Depth[newSuccessor] = depth; 
+        }
+
+        if (i < EntryBlocks.size() - 1 &&
+            (isa<JoinInst>(successor->getTerminator()) ||
+             successor == regionStart)) {
+          Prev.insert(BB);
+          Term->setSuccessor(idx, end);
+
+          PHINode *PN;
+          for (BasicBlock::iterator I = end->begin(); 
+               (PN = dyn_cast<PHINode>(I)); ++I) {
+            PN->addIncoming(PN->getIncomingValueForBlock(regionStart), BB);
+          }
+        }
+      }
+    }
+
+    for (BasicBlock *New : NewBlocks)
+      for (Instruction &I : *(New))
+        ::remapInstruction(&I, Prev, NewBlocks, *vmap, Block2Depth);
+  }
+
+  // Remove the join instruction that exits the parallel region.
+  // On a fork interior instruction the fork that starts its parallel region
+  // will handle this.
+  TerminatorInst *EndTerm = exit->getTerminator();
+  if (!(regTerm->isInterior()) && isa<JoinInst>(EndTerm)) {
+    BranchInst::Create(EndTerm->getSuccessor(0), EndTerm);
+    EndTerm->eraseFromParent();
+  }
+
+  // Only the first EntryBlock is visited by regionStart, so clear up PHIs.
+  // Don't do this until all of the new predecessors have added their PHIs 
+  for (unsigned idx = 1; idx < EntryBlocks.size(); idx++) {
+    EntryBlocks[idx]->removePredecessor(regionStart);
+  }
+
+  // Rewire regionStart to point to the first entry.
+  if (EntryBlocks.size() > 0) {
+    BranchInst::Create(EntryBlocks[0], regTerm);
+  } else {
+    BranchInst::Create(exit, regTerm);
+  }
+  regTerm->eraseFromParent();
+
+  for (ValueToValueMapTy *vmap : ValueMaps) {
+    delete vmap;
+  }
+
+  return exit;
+}
+
+// This makes the assumption that the passed in function is well-formed.
+// It seems reasonable that the compilation routine would check this, just
+// like it currently checks for well-formed PHIs, etc., but there is not
+// currently such a check.
+// Adding a dependency on the ParallelRegionInfoPass and reporting an error
+// if it finds an issue could also avoid this assumption
+bool SequentializeParallelRegions::runOnFunction(Function &F) {
+  bool modified = false;
+
+  std::deque<BasicBlock *> OpenBlocks;
+  DenseSet<BasicBlock *> SeenBlocks;
 
   OpenBlocks.push_back(&F.getEntryBlock());
   while (!OpenBlocks.empty()) {
-    BB = OpenBlocks.front();
-    PR = PRI->BB2PRMap[BB];
+    BasicBlock *BB = OpenBlocks.front();
     OpenBlocks.pop_front();
 
     if (!SeenBlocks.insert(BB).second) {
       continue;
     }
 
-    // First, check to see if the last instruction of the block is a fork.
-    // If it is, change it to a direct branch to the enty block AFTER
-    // the one that will exit.
-    // TODO: Handle fork interior.
     ForkInst *fork = dyn_cast<ForkInst>(BB->getTerminator());
-
     if (fork) {
-      PR = PRI->getForkedRegion(fork);
-      SmallVectorImpl<BasicBlock *> &SubEntrys = PR->getEntryBlocks();
-      unsigned NewEntry = (Region2EntryJoin[PR] + 1)
-        % SubEntrys.size();
-      BranchInst::Create(SubEntrys[NewEntry], BB->getTerminator());
-      // Fork instructions deleted later
-    }
-
-    SmallVectorImpl<BasicBlock *> &Entrys = PR->getEntryBlocks();
-
-    // Check if the last instruction of the block is a halt.
-    // If it is, we change it to a jump to the next entry block.
-    // TODO: To prevent infinite loops, will need to change one to unreachable
-    // if all branches lead to halt.
-    if (isa<HaltInst>(BB->getTerminator())) {
-      assert(Exit2Entry.count(BB) && "Halt in non-exit block");
-      unsigned EntryNum = Exit2Entry[BB];
-      assert(EntryNum != Region2EntryJoin[PR]);
-
-      EntryNum = (EntryNum + 1) % (Entrys.size());
-
-      BranchInst::Create(Entrys[EntryNum], BB->getTerminator());
-      BB->getTerminator()->eraseFromParent();
-      continue;
-    }
-
-    // Otherwise, we check if the first instruction of each next block is join.
-    // If it is, we change to a jump to the next remaining entry block
-    // if this is not the instruction that will leave the entry block.
-    TerminatorInst *Branch = BB->getTerminator();
-    for (unsigned idx = 0; idx < Branch->getNumSuccessors(); idx++) {
-      JoinInst *join = dyn_cast<JoinInst>(&Branch->getSuccessor(idx)->front());
-      if (join) {
-        assert(PR && "Join instruction must be inside a basic block");
-        unsigned EntryNum = Exit2Entry[BB];
-        if (EntryNum != Region2EntryJoin[PR]) {
-          EntryNum = (EntryNum + 1) % Entrys.size();
-          Branch->setSuccessor(idx, Entrys[EntryNum]);
-
-          for (BasicBlock::iterator I = Entrys[EntryNum]->begin();
-               isa<PHINode>(I); ++I) {
-            PHINode *PN = cast<PHINode>(I);
-            PN->addIncoming(PN->getIncomingValueForBlock(
-                            PR->getEntryFork()->getParent()), BB);
-          }
-        }
+      modified = true;
+      BB = SequentializeRegion(BB);
+      OpenBlocks.push_back(BB);
+    } else {
+      for (BasicBlock *successor : successors(BB)) {
+        OpenBlocks.push_back(successor);
       }
-      OpenBlocks.push_back(Branch->getSuccessor(idx));
-    }
-
-  }
-
-  // Have to wait to delete join and fork instructions
-  // because they are used to determine when a parallel region ends.
-  ForkInst *FI;
-  JoinInst *JI;
-  for (auto iter = F.begin(); iter != F.end(); ++iter) {
-    if ((JI = dyn_cast<JoinInst>(iter->begin()))) {
-      BranchInst::Create(JI->getSuccessor(0), &*iter);
-      iter->begin()->eraseFromParent();
-    }
-
-    if ((FI = dyn_cast<ForkInst>(iter->getTerminator()))) {
-      ParallelRegion *PR = PRI->getForkedRegion(FI);
-      // Clear up PHI Nodes
-      unsigned keepIndex = (Region2EntryJoin[PR] + 1) %
-                           PR->getEntryBlocks().size();
-      for (unsigned i = 0; i < FI->getNumSuccessors(); ++i) {
-        if (i != keepIndex) {
-          FI->getSuccessor(i)->removePredecessor(&(*iter));
-        }
-      }
-      FI->eraseFromParent();
     }
   }
 
-  return true;
+  if (modified) {
+    // There are some cases where different calls to SequentializeRegion can
+    // take a block down to 0 predecessors without realizing it.
+    // Clear up any resulting empty PHIs.
+    PHINode *PN;
+    for (BasicBlock &BB : F) {
+      BasicBlock::iterator it = BB.begin(); 
+      while ((PN = dyn_cast<PHINode>(it))) {
+        ++it;
+        if (PN->getNumIncomingValues() == 0) {
+          PN->replaceAllUsesWith(UndefValue::get(PN->getType()));
+          PN->eraseFromParent();
+        }
+      }
+    }
+  }
+
+  return modified;  
 }
 
 void SequentializeParallelRegions::releaseMemory() {
@@ -201,17 +365,12 @@ void SequentializeParallelRegions::dump() const {}
 #endif
 
 void SequentializeParallelRegions::getAnalysisUsage(AnalysisUsage &AU)
-  const {
-  AU.addRequiredTransitive<ParallelRegionInfoPass>();
-  AU.addRequiredTransitive<DominatorTreeWrapperPass>();
-}
+  const {}
 
 char SequentializeParallelRegions::ID = 0;
 
 INITIALIZE_PASS_BEGIN(SequentializeParallelRegions, "sequentialize-regions",
                       "Remove all fork and join instructions", true, false)
-INITIALIZE_PASS_DEPENDENCY(ParallelRegionInfoPass)
-INITIALIZE_PASS_DEPENDENCY(DominatorTreeWrapperPass)
 INITIALIZE_PASS_END(SequentializeParallelRegions, "sequentialize-regions",
                     "Remove all fork and join instructions", true, false)
 
